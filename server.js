96744a1c1826030d5a03c33dc4f3f41c51d6875a
Dhaval Trivedi <dhaval6244@gmail.com>
2023-10-29 11:44:43 +0530

    add logger middleware

--- server.js
+++ server.js
@@ -33,13 +33,18 @@ app.listen(8080, () => {
   console.log(`Server is up and running on port ${PORT}`);
 });
 
+const logger = (req, res, next) => {
+  console.log(`${req.method} request received on ${req.url}`);
+  next();
+};
+
+app.use(logger);
+
 app.get("/", (req, res) => {
-  console.log(`request received on /`);
   res.send("Hello world");
 });
 
 app.get("/api/movies", (req, res) => {
-  console.log(`requests received on /api/movies`);
   res.send(movies);
 });
 
